b217a27c78491ae37b663fdbf6852e969ee6491d
luci <wefly9294@naver.com>
2026-09-07 11:55:50 +0900

    Add Capacitor iOS setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sesac.speakconnect',
+  appName: 'Speak Connect',
+  webDir: '.output/public'
+};
+
+export default config;
